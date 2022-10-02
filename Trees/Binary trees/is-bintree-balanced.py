77c16838edfb67808ad130ad3e8fbb691a0bb15c
apoorvpandey0 <apoorvpandey0@gmail.com>
2022-10-02 17:06:45 +0530

    added code

--- Trees/Binary trees/is-bintree-balanced.py
+++ Trees/Binary trees/is-bintree-balanced.py
@@ -12,6 +12,8 @@ class TreeNode:
         self.right = right
 
 class Solution:
+    # Solution 1
+    # A bit complex
     # Runtime: 52 ms, faster than 90.23% of Python3 online submissions for Balanced Binary Tree.
     # Memory Usage: 18.9 MB, less than 22.76% of Python3 online submissions for Balanced Binary Tree.
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
@@ -25,3 +27,29 @@ class Solution:
             
             return [balanced,1+max(left[1],right[1])]
         return dfs(root)[0]
+    
+    # Solution 2
+    # Clean code
+    # Runtime: 47 ms, faster than 98.35% of Python3 online submissions for Balanced Binary Tree.
+    # Memory Usage: 18.7 MB, less than 60.84% of Python3 online submissions for Balanced Binary Tree.
+    def isBalanced2(self, root):
+        def check(root):
+            
+            # Return heght of leaf node as 0
+            if not root: return 0
+            
+            # Find height of left and right subtrees
+            lh = check(root.left)
+            rh = check(root.right)
+
+            # If any of subtrees is not balanced i.e. -1 return -1            
+            if lh == -1 or rh ==-1: return -1
+
+            # if current node is not balanced return -1
+            if abs(lh-rh)>1: return -1
+
+            # Return height +1
+            return max(lh,rh)+1
+
+        # To return a boolean value
+        return check(root)!=-1
