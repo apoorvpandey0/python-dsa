a0dd28ab906628c8d3aba4f138b3174483b0905f
Apoorv Pandey <32810722+apoorvpandey0@users.noreply.github.com>
2024-09-19 02:00:22 +0530

    Create 2sum.py

---
+++ Arrays/2sum.py
@@ -0,0 +1,6 @@
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+https://leetcode.com/problems/two-sum/description/
+
+1. Brute O(n^2)
+2. Sort -> BS O(nlogn)
+3. Set -> O(n)
