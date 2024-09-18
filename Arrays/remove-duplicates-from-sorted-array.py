b4d4548f140740442cc7ce9489e5c2674ad17990
Apoorv Pandey <32810722+apoorvpandey0@users.noreply.github.com>
2024-09-19 01:07:28 +0530

    Update remove-duplicates-from-sorted-array.py

--- Arrays/remove-duplicates-from-sorted-array.py
+++ Arrays/remove-duplicates-from-sorted-array.py
@@ -1,2 +1,15 @@
-1. Use set to store unique values and count
-2. Use two pointer method
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+# 1. Use set to store unique values and count
+# 2. Use two pointer method
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums)<2:return 1
+        l = 1
+        r = 1
+
+        while r<len(nums):
+            if nums[r-1]!=nums[r]: 
+                nums[l] = nums[r]
+                l+=1
+            r+=1
+        return l
