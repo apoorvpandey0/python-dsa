0e99403009c0ec1eac40da6b36076097efe37af5
Apoorv Pandey <32810722+apoorvpandey0@users.noreply.github.com>
2024-10-16 22:52:18 +0530

    Update majority-element-2.py

--- majority-element-2.py
+++ majority-element-2.py
@@ -14,6 +14,28 @@ def me(nums):
     c = Counter(nums)
     return list(filter(lambda x: c[x] > len(nums) / 3, c))
 
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        count = {}
+
+        for n in nums:
+            if n in count: count[n] += 1
+            else: count[n] = 1
+
+            if len(count) > 2:
+                new_count = {}
+                for n, c in count.items():
+                    if c > 1:
+                        new_count[n] = c - 1
+                count = new_count
+
+        res = []
+        for n in count:
+            if nums.count(n) > len(nums) // 3:
+                res.append(n)
+                
+        return res
+
 
 # Solution 2:
 # Using Boyre Moores algorithm
