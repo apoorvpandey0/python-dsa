692ac9312787ed8b432fe93e8d3b24b3374fa30b
Apoorv Pandey <32810722+apoorvpandey0@users.noreply.github.com>
2024-10-17 20:02:28 +0530

    Update 2sum.py

--- Arrays/2sum.py
+++ Arrays/2sum.py
@@ -4,3 +4,20 @@ https://leetcode.com/problems/two-sum/description/
 1. Brute O(n^2)
 2. Sort -> BS O(nlogn)
 3. Set -> O(n)
+
+Modification
+Find all pair values whose sum is = target
+def FindAllPairs(self, nums: List[int], target: int) -> List[int]:
+    nums.sort()
+    l = 0
+    r = len(nums)-1
+    ans = []
+    while l<r:
+        sm = nums[l] + nums[r]
+        print(l,r,sm)
+        if sm==target: 
+            ans.append([nums[l],nums[r]])
+            l+=1
+        elif sm>target: r-=1
+        elif sm<target: l+=1
+    return ans[0]
