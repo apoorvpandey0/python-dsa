66d06f7b0d866cb2379cc548fc1a9ecdf811fa5a
Apoorv Pandey <32810722+apoorvpandey0@users.noreply.github.com>
2024-12-19 00:11:03 +0530

    Update split-array-largest-sum.py

--- Binary Search/split-array-largest-sum.py
+++ Binary Search/split-array-largest-sum.py
@@ -35,5 +35,45 @@ class Solution:
             if countSubs <= k:
                 ans = min(ans,maxSubSum)
             
+        return ans
+
+================================== Optimized BS ===============================
+
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        ans = float('inf')
+        lo = max(nums)
+        hi = sum(nums)+1
+        while lo<=hi: 
+            curr = (lo+hi)//2
+            
+            subs = []
+            maxSubSum = float('-inf')
+
+            countSubs = 0
+            tmpSubArray = []
+            tmpSubArraySum = 0
+            for ele in nums:
+                if tmpSubArraySum + ele > curr:
+                    maxSubSum = max(maxSubSum,tmpSubArraySum)
+                    subs.append(tmpSubArray)
+                    tmpSubArraySum = 0
+                    countSubs += 1
+                    tmpSubArray = []
+                tmpSubArraySum += ele
+                tmpSubArray.append(ele)
+                
+            if tmpSubArray:
+                maxSubSum = max(maxSubSum, tmpSubArraySum)
+                subs.append(tmpSubArray)
+                countSubs += 1
+            
+            # print(curr,maxSubSum,countSubs,tmpSubArraySum,subs)
+
+            if countSubs <= k:
+                hi = curr - 1
+                ans = min(ans,maxSubSum)
+            else: lo = curr+1
+            
 
         return ans
