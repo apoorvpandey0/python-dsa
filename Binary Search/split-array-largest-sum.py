d75f28b21de222a1d0c7e73ade5b48481693beb7
Apoorv Pandey <32810722+apoorvpandey0@users.noreply.github.com>
2024-12-19 00:08:44 +0530

    Create split-array-largest-sum.py

---
+++ Binary Search/split-array-largest-sum.py
@@ -0,0 +1,39 @@
+https://leetcode.com/problems/split-array-largest-sum/description/
+Redo this Q
+
+
+================================== Brute forced ===============================
+TLE
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        ans = float('inf')
+
+        for curr in range(max(nums),sum(nums)+1):
+            subs = []
+            maxSubSum = float('-inf')
+
+            countSubs = 0
+            tmpSubArray = []
+            tmpSubArraySum = 0
+            for ele in nums:
+                if tmpSubArraySum + ele > curr:
+                    maxSubSum = max(maxSubSum,tmpSubArraySum)
+                    subs.append(tmpSubArray)
+                    tmpSubArraySum = 0
+                    countSubs += 1
+                    tmpSubArray = []
+                tmpSubArraySum += ele
+                tmpSubArray.append(ele)
+                
+            if tmpSubArray:
+                maxSubSum = max(maxSubSum, tmpSubArraySum)
+                subs.append(tmpSubArray)
+                countSubs += 1
+            
+            # print(curr,maxSubSum,countSubs,tmpSubArraySum,subs)
+
+            if countSubs <= k:
+                ans = min(ans,maxSubSum)
+            
+
+        return ans
