0f86cf21de696fd51f4deee566a8a16130ca7699
Apoorv Pandey <32810722+apoorvpandey0@users.noreply.github.com>
2025-01-20 01:01:01 +0530

    Update infix-postfix.py

--- Stacks/infix-postfix.py
+++ Stacks/infix-postfix.py
@@ -1,40 +1,48 @@
 https://www.geeksforgeeks.org/problems/infix-to-postfix-1587115620/1
 #User function Template for python3
-
 from collections import deque
+
 class Stack:
-    """Stack implementation using deque."""
     def __init__(self):
-        self.stack = deque()
-    
-    def push(self, item):
-        self.stack.append(item)
-    
-    def pop(self):
-        return self.stack.pop() if not self.is_empty() else None
-    
-    def peek(self):
-        return self.stack[-1] if not self.is_empty() else None
-    
+        self.items = []
+
     def is_empty(self):
-        return len(self.stack) == 0
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop() if not self.is_empty() else None
+
+    def peek(self):
+        return self.items[-1] if not self.is_empty() else None
 
 class Solution:
-    
-    #Function to convert an infix expression to a postfix expression.
-    def InfixtoPostfix(self, s):
-        pri = {'+':1,'-':1,'*':2,'/':2,''}
-        stack == Stack()
+    def InfixtoPostfix(self,expression):
+        # Priority of operators
+        precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+        stack = Stack()
         postfix = ""
-        for char in s:
-            if char == '(':
+    
+        for char in expression:
+            if char.isalnum():
+                postfix += char
+            elif char == '(':
                 stack.push(char)
-            elif char in pri:
-                if pri[char] > pri[stack[-1]]:
-                    stack.push()
-                else:
-                    # Pop until stack[-1] < pri[char]
-            elif char == ')'
-            # Pop until '('
-        return ans
-                    
+            elif char == ')':
+                # Pop until '(' is found
+                while not stack.is_empty() and stack.peek() != '(':
+                    postfix += stack.pop()
+                stack.pop()  # Remove '(' from the stack
+            elif char in precedence:
+                # Pop operators with higher or equal precedence
+                while (not stack.is_empty() and precedence.get(stack.peek(), 0) >= precedence[char]):
+                    postfix += stack.pop()
+                stack.push(char)  # Push the current operator onto the stack
+    
+        # Pop remaining operators in the stack
+        while not stack.is_empty():
+            postfix += stack.pop()
+    
+        return postfix
