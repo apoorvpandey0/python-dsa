ead16441c9f27a574d45487857535b8044a678e2
Apoorv Pandey <32810722+apoorvpandey0@users.noreply.github.com>
2025-01-29 01:32:11 +0530

    Update next-greater-ele.py

--- Stacks/Monotonic Stacks/next-greater-ele.py
+++ Stacks/Monotonic Stacks/next-greater-ele.py
@@ -1,1 +1,23 @@
 https://youtu.be/vTG32RrjJaQ?si=kqX_8QmtT8J3H34X
+https://www.youtube.com/watch?v=DtJVwbbicjQ better
+
+
+
+
+
+
+Primitive Soln: only for dictinct array elements
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = Stack()
+        nxtG = {}
+
+        for num in nums2:
+
+            while not stack.is_empty() and stack.peek() <= num:
+                popele = stack.pop()
+                nxtG[popele] = num
+            
+            stack.push(num)
+        
+        return [nxtG[ele] if ele in nxtG else -1 for ele in nums1]
