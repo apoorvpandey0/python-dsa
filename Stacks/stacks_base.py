5b70b6460eff0597e51f6fd68b36204bca96fef9
Apoorv Pandey <32810722+apoorvpandey0@users.noreply.github.com>
2025-04-12 13:04:41 +0530

    Update stacks_base.py

--- Stacks/stacks_base.py
+++ Stacks/stacks_base.py
@@ -2,14 +2,16 @@ from collections import deque
 
 class Stack:
     def __init__(self):
-        self.stack = deque()
-    def push(self,val):
-        self.stack.append(val)
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
     def pop(self):
-        return self.stack.pop()
+        return -1 if self.is_empty() else self.stack.pop()
+
+    def is_empty(self):
+        return len(self.stack) == 0 
+
     def peek(self):
-        return self.stack[-1]
-    def size(self):
-        return len(self.stack)
-    def empty(self):
-        return len(self.stack) == 0
+        return -1 if self.is_empty() else self.stack[-1]
