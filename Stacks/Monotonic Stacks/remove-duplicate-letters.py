6f7ff6c120c52fc053b288064e0e180bc7884b67
Apoorv Pandey <32810722+apoorvpandey0@users.noreply.github.com>
2025-04-24 01:07:47 +0530

    Create remove-duplicate-letters.py

---
+++ Stacks/Monotonic Stacks/remove-duplicate-letters.py
@@ -0,0 +1,8 @@
+https://leetcode.com/problems/remove-duplicate-letters/description/?envType=problem-list-v2&envId=stack
+
+The goal of this problem is to find a string answer that satisfies these conditions:
+
+the answer must contains all the letters that are there in the original string s.
+the answer must not have any duplicate character.
+the answer must be a subsequence of the original string s.
+in case multiple strings pass the above 3 conditions, return the one which is lexicographically smallest.
